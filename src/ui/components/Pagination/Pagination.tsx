c91e58e554ebe3a688c35ec36471c90c7af58c8d
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2024-08-03 19:05:47 +0200

    feat(ui): create Pagination component

--- src/ui/components/Pagination/Pagination.tsx
+++ src/ui/components/Pagination/Pagination.tsx
@@ -1,3 +1,16 @@
-export const Pagination = () => {
-  return <div>Pagination</div>;
+import { Button, ButtonStyles } from "../Button/Button";
+
+type PaginationProps = {
+  isDisabled: boolean;
+  onClick: () => void;
+};
+
+export const Pagination: React.FC<PaginationProps> = ({ isDisabled, onClick }) => {
+  return (
+    <Button type="button" onClick={onClick} styleType={ButtonStyles.SECONDARY} isDisabled={isDisabled}>
+      <span>
+        Load More <img src="arrow-right-black.svg" alt="Arrow right black" />
+      </span>
+    </Button>
+  );
 };
