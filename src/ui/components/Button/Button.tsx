8295054b101adf55f6e520be1c9b10b286b9bc07
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2024-08-03 19:05:21 +0200

    feat(ui): create button component

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,3 +1,36 @@
-export const Button = () => {
-  return <div>Button</div>;
+import styles from "./Button.module.css";
+
+export enum ButtonStyles {
+  PRIMARY = "button-primary",
+  SECONDARY = "button-secondary",
+}
+
+type Props = {
+  children?: React.ReactNode;
+  styleType?: ButtonStyles;
+  isDisabled?: boolean;
+  name?: string | undefined;
+  onClick?: () => void;
+  type?: "submit" | "reset" | "button";
+};
+
+export const Button: React.FC<Props> = ({
+  children,
+  name,
+  onClick,
+  type = "button",
+  isDisabled = false,
+  styleType = ButtonStyles.PRIMARY,
+}) => {
+  return (
+    <button
+      type={type}
+      name={name}
+      className={`${styles[styleType]} ${styles.button}`}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {children}
+    </button>
+  );
 };
--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,7 +1,5 @@
 export * from './Attribution/Attribution';
 export * from './Button/Button';
-export * from './Card/Card';
-export * from './Image/Image';
 export * from './Pagination/Pagination';
 export * from './Searchbar/Searchbar';
 export * from './Spinner/Spinner';
