c54b6b5ed230338a3ca33cb90269fe848988b6b8
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2024-08-04 22:29:16 +0200

    fix(Spinner): delete data-testid attribute

--- src/ui/components/Spinner/Spinner.tsx
+++ src/ui/components/Spinner/Spinner.tsx
@@ -6,11 +6,7 @@ type SpinnerProps = {
 
 export const Spinner = ({ visibility }: SpinnerProps) => {
   return (
-    <div
-      style={{ visibility: visibility }}
-      className={styles["fullscreen-container"]}
-      data-testid="fullscreen-container"
-    >
+    <div style={{ visibility: visibility }} className={styles["fullscreen-container"]}>
       <div className={styles["spinner-container"]} data-testid="spinner-container">
         <div className={styles.spinner} data-testid="spinner" />
       </div>
