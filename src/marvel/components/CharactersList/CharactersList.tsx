1638dd12c1406b062beeedd2ba8714544a42b1c2
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2025-03-08 15:07:59 +0100

    fix(CharactersList): remove unnecessary fragment

--- src/marvel/components/CharactersList/CharactersList.tsx
+++ src/marvel/components/CharactersList/CharactersList.tsx
@@ -1,8 +1,8 @@
-import { DEFAULT_STATE_VALUE, useCharacters } from "@marvel/hooks";
-import { Pagination, Searchbar, Spinner } from "@ui/components";
-import { useEffect, useState } from "react";
-import { CharacterCard } from "../CharacterCard/CharacterCard";
-import styles from "./CharactersList.module.css";
+import { DEFAULT_STATE_VALUE, useCharacters } from '@marvel/hooks';
+import { Pagination, Searchbar, Spinner } from '@ui/components';
+import { useEffect, useState } from 'react';
+import { CharacterCard } from '../CharacterCard/CharacterCard';
+import styles from './CharactersList.module.css';
 
 export const CharactersList: React.FC = () => {
   const {
@@ -22,7 +22,7 @@ export const CharactersList: React.FC = () => {
     setHasSearchedByName,
   } = useCharacters();
 
-  const [registerToSearch, setRegisterToSearch] = useState("");
+  const [registerToSearch, setRegisterToSearch] = useState('');
 
   useEffect(() => {
     getCharacters(10, 0);
@@ -38,61 +38,83 @@ export const CharactersList: React.FC = () => {
   };
 
   const onClickPaginationSearchResults = () => {
-    getByName(registerToSearch, recordsPerPageSearch, getFromRecordNumberSearch + 10);
+    getByName(
+      registerToSearch,
+      recordsPerPageSearch,
+      getFromRecordNumberSearch + 10
+    );
   };
 
   return (
     <>
       <Searchbar
-        placeholder="Name of character"
+        placeholder='Name of character'
         onChange={getByName}
         registerToSearch={registerToSearch}
         setRegisterToSearch={setRegisterToSearch}
         setResetSearchState={resetSearchbar}
-        inputName="searchCharacter"
+        inputName='searchCharacter'
       />
       {/* Show spinner with characters at the background when click in Pagination */}
-      <Spinner visibility={isLoading && !registerToSearch ? "visible" : "hidden"} />
+      <Spinner
+        visibility={isLoading && !registerToSearch ? 'visible' : 'hidden'}
+      />
       {!isLoading &&
         ((characters.length === 0 && searchedCharacterResults.length === 0) ||
-          (hasSearchedByName && searchedCharacterResults.length === 0)) && <div>No Results</div>}
+          (hasSearchedByName && searchedCharacterResults.length === 0)) && (
+          <div>No Results</div>
+        )}
 
-      {characters.length > 0 && searchedCharacterResults.length === 0 && (
-        <>
-          {characters.length > 0 && searchedCharacterResults.length === 0 && !hasSearchedByName && (
-            <>
-              <div className={styles.charactersList}>
-                {characters.map((character) => {
-                  const cardData = {
-                    name: character?.name ?? "",
-                    description: character?.description || "(No description available)",
-                    imageData: character?.thumbnail ?? { path: undefined, extension: undefined },
-                    linkToMoreInfo: character?.urls?.find((url) => url.type === "comiclink")?.url ?? "", // setted comiclink because detail and wiki urls are not working properly in the Marvel API
-                  };
+      {characters.length > 0 &&
+        searchedCharacterResults.length === 0 &&
+        characters.length > 0 &&
+        searchedCharacterResults.length === 0 &&
+        !hasSearchedByName && (
+          <>
+            <div className={styles.charactersList}>
+              {characters.map((character) => {
+                const cardData = {
+                  name: character?.name ?? '',
+                  description:
+                    character?.description || '(No description available)',
+                  imageData: character?.thumbnail ?? {
+                    path: undefined,
+                    extension: undefined,
+                  },
+                  linkToMoreInfo:
+                    character?.urls?.find((url) => url.type === 'comiclink')
+                      ?.url ?? '', // setted comiclink because detail and wiki urls are not working properly in the Marvel API
+                };
 
-                  return <CharacterCard key={character.id} character={cardData} />;
-                })}
-              </div>
-              <div className={styles.pagination}>
-                <Pagination
-                  isDisabled={isLoading ? true : !areMoreCharactersAvailable}
-                  onClick={onClickPaginationCharacters}
-                />
-              </div>
-            </>
-          )}
-        </>
-      )}
+                return (
+                  <CharacterCard key={character.id} character={cardData} />
+                );
+              })}
+            </div>
+            <div className={styles.pagination}>
+              <Pagination
+                isDisabled={isLoading ? true : !areMoreCharactersAvailable}
+                onClick={onClickPaginationCharacters}
+              />
+            </div>
+          </>
+        )}
 
       {searchedCharacterResults.length > 0 && (
         <>
           <div className={styles.charactersList}>
             {searchedCharacterResults.map((character) => {
               const cardData = {
-                name: character?.name ?? "",
-                description: character?.description || "(No description available)",
-                imageData: character?.thumbnail ?? { path: undefined, extension: undefined },
-                linkToMoreInfo: character?.urls?.find((url) => url.type === "comiclink")?.url ?? "", // setted comiclink because detail and wiki urls are not working properly in the Marvel API
+                name: character?.name ?? '',
+                description:
+                  character?.description || '(No description available)',
+                imageData: character?.thumbnail ?? {
+                  path: undefined,
+                  extension: undefined,
+                },
+                linkToMoreInfo:
+                  character?.urls?.find((url) => url.type === 'comiclink')
+                    ?.url ?? '', // setted comiclink because detail and wiki urls are not working properly in the Marvel API
               };
 
               return <CharacterCard key={character.id} character={cardData} />;
@@ -100,7 +122,9 @@ export const CharactersList: React.FC = () => {
           </div>
           <div className={styles.pagination}>
             <Pagination
-              isDisabled={isLoading ? true : !areMoreCharactersSearchedAvailable}
+              isDisabled={
+                isLoading ? true : !areMoreCharactersSearchedAvailable
+              }
               onClick={onClickPaginationSearchResults}
             />
           </div>
